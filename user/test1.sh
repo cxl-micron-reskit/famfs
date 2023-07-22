ed429021e2902c7e809e36104a2cf8c1f349e772
John Groves <john@groves.net>
2023-07-22 18:26:52 -0500

    test1.sh - files (barely) larger than a page

---
+++ user/test1.sh
@@ -0,0 +1,22 @@
+#/usr/bin/bash
+
+fail () {
+    set +x
+    echo
+    echo "*** Fail ***"
+    echo "$1"
+    echo
+    exit 1
+}
+
+cwd=$(pwd)
+export PATH=cwd/debug:$PATH
+
+DEV=/dev/pmem0
+MPT=/mnt/tagfs
+
+set -x
+
+sudo debug/tagfs creat -r -s 8192 -S 10 -f $MPT/test10   || fail "creat test1"
+sudo debug/tagfs verify -S 10 -f $MPT/test10 || fail "verify test1 after replay"
+
