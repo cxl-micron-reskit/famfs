cd74019a31e89207167896b6b4784767ce8d1c68
John Groves <john@groves.net>
2023-07-23 19:54:56 -0500

    test2.sh working

--- user/test1.sh
+++ user/test1.sh
@@ -17,8 +17,23 @@ MPT=/mnt/tagfs
 
 set -x
 
-sudo debug/tagfs creat -r -s 8192 -S 10 -f $MPT/test10   || fail "creat test1"
-sudo debug/tagfs verify -S 10 -f $MPT/test10 || fail "verify test1 after replay"
+#
+# Do stuff with files bigger than a page, cautiously
+#
+
+F=test10
+sudo debug/tagfs creat -r -s 8192 -S 10 -f $MPT/$F   || fail "creat $F"
+sudo debug/tagfs verify -S 10 -f $MPT/$F || fail "verify $F after replay"
+
+F=bigtest0
+sudo debug/tagfs creat -r -S 42 -s 0x800000 -f $MPT/$F   || fail "creat $F"
+# sudo debug/tagfs clone $MPT/bigtest0 $MPT/bigtest0_clone        || fail "clone bigtest0"
+sudo debug/tagfs verify -S 42 -f $MPT/$F                 || fail "$F mismatch"
+#sudo debug/tagfs verify -S 42 $MPT/bigtest0_clone || fail "bigtest0_clone mismatch"
+
+sudo debug/tagfs cp $MPT/$F $MPT/${F}_cp      || fail "cp $F"
+sudo debug/tagfs verify -S 42 -f $MPT/${F}_cp || fail "verify ${F}_cp"
+
 
 set +x
 echo "*************************************************************************************"
