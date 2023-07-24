3d11af0247d57197134eda954f5b44036358750b
John Groves <john@groves.net>
2023-07-23 19:35:37 -0500

    Drop manual extent list support from 'tagfs creat'. Use 'tagfs clone' instead

--- user/tagfs_cli.c
+++ user/tagfs_cli.c
@@ -754,15 +754,10 @@ tagfs_creat_usage(int   argc,
 int
 do_tagfs_cli_creat(int argc, char *argv[])
 {
-	struct tagfs_simple_extent *ext_list;
-	int c, i, rc, fd;
+	int c, rc, fd;
 	char *filename = NULL;
 	char fullpath[PATH_MAX];
 
-	int num_extents = 0;
-	int cur_extent  = 0;
-
-	size_t ext_size;
 	size_t fsize = 0;
 	size_t ext_list_size = 0;
 	int arg_ct = 0;
@@ -777,9 +772,6 @@ do_tagfs_cli_creat(int argc, char *argv[])
 	/* XXX can't use any of the same strings as the global args! */
 	struct option creat_options[] = {
 		/* These options set a flag. */
-		{"address",     required_argument, &verbose_flag,  'o' },
-		{"length",      required_argument, &verbose_flag,  'l'},
-		{"num_extents", required_argument,             0,  'n'},
 		{"filename",    required_argument,             0,  'f'},
 		{"size",        required_argument,             0,  's'},
 		{"seed",        required_argument,             0,  'S'},
@@ -806,7 +798,7 @@ do_tagfs_cli_creat(int argc, char *argv[])
 	/* Note: the "+" at the beginning of the arg string tells getopt_long
 	 * to return -1 when it sees something that is not recognized option
 	 * (e.g. the command that will mux us off to the command handlers */
-	while ((c = getopt_long(argc, argv, "+n:o:l:f:s:S:rh?",
+	while ((c = getopt_long(argc, argv, "+f:s:S:rh?",
 				creat_options, &optind)) != EOF) {
 		/* printf("optind:argv = %d:%s\n", optind, argv[optind]); */
 
@@ -816,49 +808,6 @@ do_tagfs_cli_creat(int argc, char *argv[])
 
 		arg_ct++;
 		switch (c) {
-
-		case 'n':
-			num_extents = atoi(optarg);
-			if (num_extents > 0) {
-				ext_list = calloc(num_extents, sizeof(*ext_list));
-			} else {
-				fprintf(stderr, "Specify at least 1 extent\n");
-				exit(-1);
-			}
-			break;
-
-		case 'o':
-			if (num_extents == 0) {
-				fprintf(stderr,
-					"Must specify num_extents before address or offset\n");
-				exit(-1);
-			}
-			ext_list[cur_extent].tagfs_extent_offset = strtoull(optarg, 0, 0);
-
-			/* update cur_extent if we already have len */
-			if (ext_list[cur_extent].tagfs_extent_len)
-				cur_extent++;
-			break;
-			
-		case 'l':
-			if (num_extents == 0) {
-				fprintf(stderr,
-					"Must specify num_extents before length\n");
-				exit(-1);
-			}
-			ext_size = strtoull(optarg, 0, 0);
-			if (ext_size <= 0) {
-				fprintf(stderr, "invalid extent size %ld\n",
-					ext_size);
-				exit(-1);
-			}
-			ext_list[cur_extent].tagfs_extent_len = ext_size;
-			ext_list_size += ext_size;
-
-			/* update cur_extent if we already have offset */
-			if (ext_list[cur_extent].tagfs_extent_offset)
-				cur_extent++;
-			break;
 
 		case 'f': {
 			filename = optarg;
@@ -907,13 +856,6 @@ do_tagfs_cli_creat(int argc, char *argv[])
 		exit(-1);
 	}
 
-	if (num_extents > 0) {
-		printf("%d extents specified:\n", num_extents);
-		printf("Total size: %ld\n", fsize);
-		for (i=0; i<num_extents; i++)
-			printf("\t%p\t%lld\n", (void *)ext_list[i].tagfs_extent_offset,
-			       ext_list[i].tagfs_extent_len);
-	}
 	if (ext_list_size && fsize) {
 		if (fsize > ext_list_size) {
 			fprintf(stderr, "error: fsize(%ld) > ext_list_size (%ld)\n",
@@ -933,67 +875,12 @@ do_tagfs_cli_creat(int argc, char *argv[])
 			__func__, filename);
 	}
 
-	if (num_extents > 0) {
-		struct tagfs_log *logp;
-		char mpt_out[PATH_MAX];
-		size_t log_size;
-		char *relpath;
-		void *addr;
-		int lfd;
-
-		/*
-		 * Extent list was specified, so it's not coming from the official
-		 * allocator Log the allocation anyway, which can be used for
-		 * testing duplicate allocations, etc.  So we need to do this manually.
-		 */
-
-		lfd = open_log_file_writable(fullpath, &log_size, mpt_out);
-		addr = mmap(0, log_size, O_RDWR, MAP_SHARED, lfd, 0);
-		if (addr == MAP_FAILED) {
-			fprintf(stderr, "%s: Failed to mmap log file", __func__);
-			close(lfd);
-			return -1;
-		}
-		close(lfd);
-		logp = (struct tagfs_log *)addr;
-
-		relpath = tagfs_relpath_from_fullpath(mpt_out, fullpath);
-		if (!relpath)
-			return -EINVAL;
-
-		/*
-		 * Create the file before logging, so we can avoid a BS log entry if the
-		 * kernel rejects the caller-supplied allocation ext list
-		 */
-		rc = tagfs_file_map_create(fullpath, fd, fsize, num_extents,
-					   ext_list, TAGFS_REG);
-		if (rc) {
-			fprintf(stderr,
-				"%s: failed to create file with caller-specified allocation\n",
-				__func__);
-			exit(-1);
-		}
-
-		/* XXX - tagfs_log_file_creation should only be called outside
-		 * tagfs_lib.c if we are intentionally doing extent list allocation
-		 * bypassing tagfs_lib. This is useful for testing, by generating
-		 * problematic extent lists on purpoose...
-		 */
-		rc = tagfs_log_file_creation(logp, num_extents, ext_list,
-					     relpath, O_RDWR, uid, gid, fsize);
-		if (rc) {
-			fprintf(stderr,
-				"%s: failed to log caller-specified allocation\n",
-				__func__);
-			exit(-1);
-		}
-	} else {
-		rc = tagfs_file_alloc(fd, fullpath, O_RDWR, uid, gid, fsize);
-		if (rc) {
-			fprintf(stderr, "%s: tagfs_file_alloc(%s, size=%ld) failed\n",
-				__func__, fullpath, fsize);
-		}
+	rc = tagfs_file_alloc(fd, fullpath, O_RDWR, uid, gid, fsize);
+	if (rc) {
+	  fprintf(stderr, "%s: tagfs_file_alloc(%s, size=%ld) failed\n",
+		  __func__, fullpath, fsize);
 	}
+
 	if (randomize) {
 		struct stat st;
 		void *addr;
