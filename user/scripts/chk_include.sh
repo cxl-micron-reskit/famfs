70503a29f462702f6b2adcae090e5ebb456a2a20
John Groves <john@groves.net>
2024-02-04 10:25:42 -0600

    make scripts/chk_include.sh smarter

--- user/scripts/chk_include.sh
+++ user/scripts/chk_include.sh
@@ -1,8 +1,25 @@
 
-INC="/usr/include/linux/famfs_ioctl.h"
-if [ ! -f $INC ]; then
-    echo "$INC not found"
-    echo "You need to install a famfs kernel"
-    exit -1
+#!/usr/bin/env bash
+
+kpath=/lib/modules/$(uname -r)
+#instpath=$kpath/extra
+#famfs=../kmod/famfs.ko
+
+incpath=/usr/include/linux/
+
+
+#
+# If there's an in-tree kmod, make sure /usr/include/linux/famfs_ioctl.h matches it
+#
+if [ -f "$kpath/kernel/fs/famfs/famfs.ko" ]; then
+    echo "famfs is part of the installed kernel; installing kernel famfs_ioctl.h"
+    sudo cp $kpath/include/uapi/linux/famfs_ioctl.h $incpath
+    exit 0
 fi
+
+#
+# Otherwise copy famfs_ioctl.h from local kmod directory
+#
+echo "famfs not part of installed kernel; installing local famfs_ioctl.h"
+sudo cp ../kmod/famfs_ioctl.h $incpath
 exit 0
