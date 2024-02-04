9edb1474dc898d04d9da5205ee1e20244a8daeca
John Groves <john@groves.net>
2024-02-04 06:41:26 -0600

    fc_print.sh - fix stale sysfs location of fault counters

--- user/scripts/fc_print.sh
+++ user/scripts/fc_print.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
 echo -n "pte faults: "
-sudo cat /sys/kernel/famfs/pte_fault_ct
+sudo cat /sys/fs/famfs/pte_fault_ct
 echo
 echo -n "pmd faults: "
-sudo cat /sys/kernel/famfs/pmd_fault_ct
+sudo cat /sys/fs/famfs/pmd_fault_ct
 echo
 echo -n "pud faults: "
-sudo cat /sys/kernel/famfs/pud_fault_ct
+sudo cat /sys/fs/famfs/pud_fault_ct
 echo
