18cd4d135857860a836b3febd8e2db00506f7a1b
John Groves <john@groves.net>
2024-02-17 12:50:56 -0600

    Bug fix from Aravind

--- scripts/chk_include.sh
+++ scripts/chk_include.sh
@@ -1,19 +1,15 @@
-
 #!/usr/bin/env bash
 
 kpath=/lib/modules/$(uname -r)
-#instpath=$kpath/extra
-#famfs=../kmod/famfs.ko
 
 incpath=/usr/include/linux/
-
 
 #
 # If there's an in-tree kmod, make sure /usr/include/linux/famfs_ioctl.h matches it
 #
 if [ -f "$kpath/kernel/fs/famfs/famfs.ko" ]; then
     echo "famfs is part of the installed kernel; installing kernel famfs_ioctl.h"
-    sudo cp $kpath/include/uapi/linux/famfs_ioctl.h $incpath
+    sudo cp $kpath/build/include/uapi/linux/famfs_ioctl.h $incpath
     exit 0
 fi
 
