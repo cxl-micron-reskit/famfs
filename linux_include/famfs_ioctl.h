cf98f09f30f4753f0d2deb3d0ff46d5aa26022e9
John Groves <john@groves.net>
2024-10-16 16:31:29 -0500

    Update famfs_ioctl.h for github integration tests

--- linux_include/famfs_ioctl.h
+++ linux_include/famfs_ioctl.h
@@ -17,22 +17,7 @@
 #define FAMFS_KABI_VERSION 43
 #define FAMFS_MAX_EXTENTS 2
 #define FAMFS_MAX_STRIPS 16
-#define FAMFS_IOC_MAX_INTERLEAVED_EXTENTS 1
-/*#define FAMFS_IOC_MAX_INTERLEAVED_STRIPS 8*/
-
-/* KABI version 42 (aka v1) */
-
-/* We anticipate the possiblity of supporting additional types of extents */
-enum famfs_extent_type {
-	SIMPLE_DAX_EXTENT,
-	INTERLEAVED_EXTENT,
-	INVALID_EXTENT_TYPE,
-};
-
-struct famfs_extent {
-	__u64              offset;
-	__u64              len;
-};
+#define FAMFS_IOC_MAX_INTERLEAVED_EXTENTS 4
 
 enum famfs_file_type {
 	FAMFS_REG,
@@ -40,23 +25,78 @@ enum famfs_file_type {
 	FAMFS_LOG,
 };
 
-/**
- * struct famfs_ioc_map - the famfs per-file metadata structure
- * @extent_type: what type of extents are in this ext_list
- * @file_type: Mark the superblock and log as special files. Maybe more later.
- * @file_size: Size of the file, which is <= the size of the ext_list
- * @ext_list_count: Number of extents
- * @ext_list: 1 or more extents
+/* KABI version 43 (aka v2) fmap structures
+ *
+ * The location of the memory backing for a famfs file is described by
+ * struct famfs_ioc_fmap, which specifies the file_size, file_type and extent_type,
+ * Followed by an extent list of the specified type.
+ *
+ * There are currently two extent formats: Simple and Interleaved.
+ *
+ * Simple extents are just (devindex, offset, length) tuples, where devindex
+ * references a devdax device that must already be "registered" with famfs.
+ * As of 10/2024, famfs only supports one devdax device (which is registered as
+ * the backing device at mount time), so devindex=0. But multiple backing devices
+ * is coming soon.
+ *
+ * The extent list size must be >= file_size.
+ *
+ * Interleaved extents merit some additional explanation. Interleaved extents
+ * stripe data across a collection of strips. Each strip is a contiguous allocation
+ * from a single devdax device - and is described by a simple_extent structure.
+ *
+ * Interleaved_extent example:
+ *   ie_nstrips = 4
+ *   ie_chunk_size = 2MiB
+ *   ie_nbytes = 32MiB
+ *
+ * ┌────────────┐────────────┐────────────┐────────────┐
+ * │Chunk = 0   │Chunk = 1   │Chunk = 2   │Chunk = 3   │
+ * │Strip = 0   │Strip = 1   │Strip = 2   │Strip = 3   │
+ * │Stripe = 0  │Stripe = 0  │Stripe = 0  │Stripe = 0  │
+ * │            │            │            │            │
+ * └────────────┘────────────┘────────────┘────────────┘
+ * │Chunk = 4   │Chunk = 5   │Chunk = 6   │Chunk = 7   │
+ * │Strip = 0   │Strip = 1   │Strip = 2   │Strip = 3   │
+ * │Stripe = 1  │Stripe = 1  │Stripe = 1  │Stripe = 1  │
+ * │            │            │            │            │
+ * └────────────┘────────────┘────────────┘────────────┘
+ * │Chunk = 8   │Chunk = 9   │Chunk = 10  │Chunk = 11  │
+ * │Strip = 0   │Strip = 1   │Strip = 2   │Strip = 3   │
+ * │Stripe = 2  │Stripe = 2  │Stripe = 2  │Stripe = 2  │
+ * │            │            │            │            │
+ * └────────────┘────────────┘────────────┘────────────┘
+ *
+ * * Data is laid out across chunks in chunk # order
+ * * Columns are strips
+ * * Rows are stripes
+ * * The number of chunks is (int)((file_size + chunk_size - 1) / chunk_size)
+ *   (and obviously the last chunk could be partial)
+ * * The stripe_size = (nstrips * chunk_size)
+ * * chunk_num(offset) = offset / chunk_size
+ * * strip_num(offset) = chunk_num(offset) % nchunks
+ * * stripe_num(offset) = offset / stripe_size
+ * * ...You get the idea - see the code for more details...
+ *
+ * Some concrete examples:
+ * * Offset 0 in the file is offset 0 in chunk 0, which is offset 0 in strip 0
+ * * offset 4MiB in the file is offset 0 in chunk 2, which is offset 0 in
+ *   strip 2
+ * * Offset 15MiB in the file is offset 1MiB in chunk 7, which is offset 3MiB
+ *   in strip 4
+ *
+ * Notes about this metadata format:
+ *
+ * * For various reasons, chunk_size must be a multiple of the applicable
+ *   PAGE_SIZE
+ * * Since chunk_size and nstrips are constant within an interleaved_extent,
+ *   resolving a file offset to a strip offset within a single interleaved_ext
+ *   is order 1.
+ * * If nstrips==1, a list of interleaved_ext structures degenerates to a
+ *   regular extent list (albeit with some wasted struct space). As such, we
+ *   could drop the union in famfs_ioc_fmap and just keep the array of
+ *   famfs_ioc_interleaved_ext.
  */
-struct famfs_ioc_map {
-	enum famfs_extent_type    extent_type;
-	enum famfs_file_type      file_type;
-	__u64                     file_size;
-	__u64                     ext_list_count;
-	struct famfs_extent       ext_list[FAMFS_MAX_EXTENTS];
-};
-
-/* KABI version 43 (aka v2) fmap structures */
 enum famfs_ioc_ext_type {
 	FAMFS_IOC_EXT_SIMPLE,
 	FAMFS_IOC_EXT_INTERLEAVE,
@@ -71,7 +111,7 @@ struct famfs_ioc_simple_extent {
 struct famfs_ioc_interleaved_ext {
 	__u64 ie_nstrips;
 	__u64 ie_chunk_size;
-	__u64 ie_nbytes;     /* Total bytes for this interleaved_ext; sum of strips may be more */
+	__u64 ie_nbytes; /* Total bytes for this interleaved_ext; sum of strips may be more */
 	struct famfs_ioc_simple_extent *ie_strips;
 };
 
@@ -81,14 +121,48 @@ struct famfs_ioc_fmap {
 	__u32 fioc_ext_type; /* enum famfs_log_ext_type */
 	union {  /* Make code a little more readable */
 		struct {
-			__u32 fioc_nextents;
+			__u32 fioc_nextents; /* the number of simple extents */
 			struct famfs_ioc_simple_extent *kse;     /* simple extent list */
 		};
 		struct {
-			__u32 fioc_niext;
+			__u32 fioc_niext; /* the number of interleaved extents */
 			struct famfs_ioc_interleaved_ext *kie; /* interleaved ext list */
 		};
 	};
+};
+
+/* KABI version 42 (aka v1)
+ *
+ * This metadata is temporarily maintained for backwards compatibility with
+ * down-level user space. Otherwise it should be ignored
+ */
+
+/* We anticipate the possiblity of supporting additional types of extents */
+enum famfs_extent_type {
+	SIMPLE_DAX_EXTENT,
+	INTERLEAVED_EXTENT,
+	INVALID_EXTENT_TYPE,
+};
+
+struct famfs_extent {
+	__u64              offset;
+	__u64              len;
+};
+
+/**
+ * struct @famfs_ioc_map - the famfs per-file metadata structure
+ * @extent_type: what type of extents are in this ext_list
+ * @file_type: Mark the superblock and log as special files. Maybe more later.
+ * @file_size: Size of the file, which is <= the size of the ext_list
+ * @ext_list_count: Number of extents
+ * @ext_list: 1 or more extents
+ */
+struct famfs_ioc_map {
+	enum famfs_extent_type    extent_type;
+	enum famfs_file_type      file_type;
+	__u64                     file_size;
+	__u64                     ext_list_count;
+	struct famfs_extent       ext_list[FAMFS_MAX_EXTENTS];
 };
 
 /**
