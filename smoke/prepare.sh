3547e0774f9b1774ffa2f48f5d27acc54e60b8a1
John Groves <John@Groves.net>
2025-04-08 08:29:58 -0500

    smoke: fix longstanding bug in prepare.sh
    We technically don't know whether there is a file system on the device
    or not when prepare.sh gets run. This fixes a bug in which prepare.sh
    failed UNLESS there was a valid file system.

--- smoke/prepare.sh
+++ smoke/prepare.sh
@@ -179,7 +179,10 @@ ${MKFS}  $DEV         && fail "mkfs while mounted should fail"
 #
 sudo umount $MPT       || fail "umount $MPT should succeed"
 verify_not_mounted $DEV $MPT "umount failed?"
+# We don't know for certain whether there is a valid file system, so create one...
+${MKFS} -f $DEV        || fail "mkfs should succeed wtih --force"
 ${MKFS} -f -k $DEV     || fail "mkfs/kill should succeed wtih --force"
+# Now there is not a valid file system because we killed the superblock...
 ${MOUNT} $DEV $MPT     && fail "famfs mount should fail with invalid superblock"
 ${MKFS} $DEV           || fail "clean mkfs should succeed"
 ${MOUNT} $DEV $MPT     || fail "mount of clean file system should succeed"
