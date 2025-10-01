03c76b25badfa1191e3c269cf043e3a6ea2e6fbb
John Groves <John@Groves.net>
2025-10-01 18:16:04 -0500

    famfs_mount_fuse: meta file timeouts were too short for huge daxdevs

--- src/famfs_mount.c
+++ src/famfs_mount.c
@@ -754,7 +754,7 @@ famfs_mount_fuse(
 	 * (famfs_fused) has discovered the superblock meta file)
 	 */
 	if (check_file_exists(realmpt, ".meta/.superblock",
-			      3 /* timeout */,
+			      10 /* timeout */,
 			      FAMFS_SUPERBLOCK_SIZE, &sb_size, verbose + 1)) {
 		fprintf(stderr, "%s: superblock file failed to appear\n",
 			__func__);
@@ -809,7 +809,7 @@ famfs_mount_fuse(
 	/* Wait for the log file to appear before playing the log
 	 */
 	if (check_file_exists(realmpt, ".meta/.log",
-			      3 /* timeout */,
+			      10 /* timeout */,
 			      sb->ts_log_len, &log_size_out, verbose)) {
 		fprintf(stderr, "%s: superblock file failed to appear\n",
 			__func__);
