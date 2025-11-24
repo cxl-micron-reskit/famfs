e98da5f659967cd9dd2ef80979a2c8ccdaed8f4f
John Groves <John@Groves.net>
2025-11-24 11:11:40 -0600

    test_header.sh: common header sourced by all smoke test files
    This lifts the [almost] identical headers from the smoke files, and
    eliminates the problem of keeping that in sync.

--- smoke/test_fio.sh
+++ smoke/test_fio.sh
@@ -1,94 +1,11 @@
 #!/usr/bin/env bash
 
-cwd=$(pwd)
+source ./test_header.sh
 
-# Defaults
-VG=""
-SCRIPTS=../scripts
-RAW_MOUNT_OPTS="-t famfs -o noatime -o dax=always "
-BIN=../debug
-VALGRIND_ARG="valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes"
-RMMOD=0
-FAMFS_MOD="famfs.ko"
-
-# Allow these variables to be set from the environment
-if [ -z "$DEV" ]; then
-    DEV="/dev/dax0.0"
-fi
-if [ -z "$MPT" ]; then
-    MPT=/mnt/famfs
-fi
-if [ -z "$UMOUNT" ]; then
-    UMOUNT="umount"
-fi
-if [ -z "${FAMFS_MODE}" ]; then
-    FAMFS_MODE="v1"
-fi
-
-# Override defaults as needed
-while (( $# > 0)); do
-    flag="$1"
-    shift
-    case "$flag" in
-	(-M|--module)
-	    FAMFS_MOD=$1
-	    shift
-	    ;;
-	(-d|--device)
-	    DEV=$1
-	    shift;
-	    ;;
-	(-b|--bin)
-	    BIN=$1
-	    shift
-	    ;;
-	(-s|--scripts)
-	    SCRIPTS=$1
-	    source_root=$1;
-	    shift;
-	    ;;
-	(-m|--mode)
-	    FAMFS_MODE="$1"
-	    shift
-	    ;;
-	(-v|--valgrind)
-	    # no argument to -v; just setup for Valgrind
-	    VG=${VALGRIND_ARG}
-	    ;;
-	(-n|--no-rmmod)
-	    RMMOD=0
-	    ;;
-	*)
-	    echo "Unrecognized command line arg: $flag"
-	    ;;
-
-    esac
-done
-
-if [[ "$FAMFS_MODE" == "v1" || "$FAMFS_MODE" == "fuse" ]]; then
-    echo "FAMFS_MODE: $FAMFS_MODE"
-    if [[ "$FAMFS_MODE" == "fuse" ]]; then
-	MOUNT_OPTS="--fuse" # Can drop this b/c fuse is the default
-	MKFS_OPTS="--nodax"
-	FSCK_OPTS="--nodax"
-    else
-        MOUNT_OPTS="--nofuse" # Can drop this b/c fuse is the default
-	MKFS_OPTS=""
-	FSCK_OPTS=""
-    fi
-else
-    echo "FAMFS_MODE: invalid"
-    exit 1;
-fi
-
-MOUNT="sudo $VG $BIN/famfs mount $MOUNT_OPTS"
-MKFS="sudo $VG $BIN/mkfs.famfs $MKFS_OPTS"
-CLI="sudo $VG $BIN/famfs"
-FSCK="${CLI} fsck $FSCK_OPTS"
 TEST="test_fio"
 
 source $SCRIPTS/test_funcs.sh
-# Above this line should be the same for all smoke tests
+
 echo "SCRIPTS=$SCRIPTS $(realpath $SCRIPTS)"
 REALSCRIPTS=$(realpath $SCRIPTS)
 
